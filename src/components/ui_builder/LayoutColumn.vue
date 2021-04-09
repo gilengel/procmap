78b6db164a7f0deb3eb3425dc2f5141996ab8014
gilengel <gil.engel@web.de>
2021-04-09 23:09:22 +0200

    Rework on row layout component

--- src/components/ui_builder/LayoutColumn.vue
+++ src/components/ui_builder/LayoutColumn.vue
@@ -1,32 +1,18 @@
 <template>
-  <div class="row">
-    <div class="col" @contextmenu="showMenu($event)">1</div>
-    <div class="col">2</div>
-    <div class="col">3</div>
+  <div style="padding: 1em">
+    <div class="row" ref="container">
+      <div :class="colClass(i)" v-for="(n, i) in columns">
+        <h1 style="color: salmon; font-size: 1em">{{colSizes[i]}}</h1>
+        <q-btn flat round color="white" icon="las la-trash-alt" />
+      </div>
 
-    <q-menu
-      v-model="menuVisible"
-      anchor="top left"
-      self="top left"
-      :offset="menuOffset"
-    >
-      <q-list style="width: 200px">
-        <q-item clickable v-close-popup>
-          <q-item-section avatar>
-            <q-icon color="primary" name="las la-columns" />
-          </q-item-section>
-
-          <q-item-section>Split</q-item-section>
-        </q-item>
-        <q-item clickable v-close-popup>
-          <q-item-section avatar>
-            <q-icon color="primary" name="las la-trash" />
-          </q-item-section>
-
-          <q-item-section>Delete</q-item-section>
-        </q-item>
-      </q-list>
-    </q-menu>
+      <div
+        class="splitter"
+        :style="splitterStyle(i)"
+        v-for="(n, i) in columns - 1"
+        @mousedown="dragMouseDown($event, i)"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -37,19 +23,125 @@ import { Vue, Component, Prop } from "vue-property-decorator";
   name: "LayoutColumn",
 })
 export default class LayoutColumn extends Vue {
-  menuVisible = false;
-  value = 0;
+  @Prop({ default: 2 }) minColSize!: number;
+  @Prop({ default: 4 }) maxColSize!: number;
 
-  menuOffset = [0, 0];
+  columns = 4;
+  colSizes = new Array<number>();
 
-  showMenu(e: MouseEvent) {
-    e.preventDefault();
+  flexColumns = 12;
 
-    const parentBoundingBox = (e.target as HTMLElement).getBoundingClientRect();
-    Vue.set(this.menuOffset, 0, -(e.clientX - parentBoundingBox.x) + 100);
-    Vue.set(this.menuOffset, 1, -(e.clientY - parentBoundingBox.y) + 16);
+  selectedSplitter: HTMLElement | null = null;
+  selectedSplitterIndex: number = -1;
 
-    this.menuVisible = true;
+  factor = 100 / this.columns;
+
+  positions = {
+    clientX: 0,
+    clientY: 0,
+    movementX: 0,
+    movementY: 0,
+  };
+
+  previosColSize(index: number): number {
+    let result = 0;
+    for (let i = 0; i < index; i++) {
+      result += this.colSizes[i];
+    }
+
+    return result;
+  }
+
+  colClass(i: number): string {
+    return `col col-${this.colSizes[i]}`;
+  }
+
+  splitterStyle(i: number): string {
+    return `left: ${(i + 1) * this.factor}%`;
+  }
+
+  mounted() {
+    for (let i = 0; i < this.columns; i++) {
+      this.colSizes.push(this.flexColumns / this.columns);
+    }
+  }
+
+  dragMouseDown(event: MouseEvent, index: number) {
+    event.preventDefault();
+    // get the mouse cursor position at startup:
+    this.positions.clientX = event.clientX;
+    this.positions.clientY = event.clientY;
+    document.onmousemove = this.elementDrag;
+    document.onmouseup = this.closeDragElement;
+
+    this.selectedSplitter = event.target as HTMLElement;
+    this.selectedSplitterIndex = index;
+  }
+
+  private containerWidth(): number {
+    return (this.$refs.container as HTMLElement).getBoundingClientRect().width;
+  }
+
+  private updatePositions(event: MouseEvent) {
+    this.positions.movementX = this.positions.clientX - event.clientX;
+    this.positions.movementY = this.positions.clientY - event.clientY;
+    this.positions.clientX = event.clientX;
+    this.positions.clientY = event.clientY;
+  }
+
+  elementDrag(event: MouseEvent) {
+    event.preventDefault();
+
+    this.updatePositions(event);
+
+    if (this.selectedSplitter) {
+      const el = this.selectedSplitter;
+      const relativeLeft = el.offsetLeft - this.positions.movementX;
+      const containerWidth = this.containerWidth();
+
+      let flexSize = Math.ceil(
+        (relativeLeft / containerWidth) * this.flexColumns
+      );
+
+      const leftColSize = this.colSizes[this.selectedSplitterIndex];
+      const rightColSize = this.colSizes[this.selectedSplitterIndex + 1];
+      const completeSize = leftColSize + rightColSize;
+
+      const previousColSizes = this.previosColSize(this.selectedSplitterIndex);
+      flexSize -= previousColSizes;
+
+      if (flexSize < this.minColSize) {
+        flexSize = this.minColSize;
+      }
+
+      if (flexSize > completeSize - 1) {
+        flexSize = completeSize - 1;
+      }
+
+    if (flexSize > this.maxColSize) {
+        flexSize = this.maxColSize;
+      }
+
+      Vue.set(this.colSizes, this.selectedSplitterIndex, flexSize);
+      let rightNextSize = completeSize - flexSize;
+
+      Vue.set(this.colSizes, this.selectedSplitterIndex + 1, rightNextSize);
+
+      el.style.left = `${relativeLeft}px`;
+    }
+  }
+
+  closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+    const previousColSizes =
+      this.previosColSize(this.selectedSplitterIndex + 1) / this.flexColumns;
+    const el = this.selectedSplitter;
+
+    if (el) {
+      el.style.left = `${previousColSizes * this.containerWidth()}px`;
+    }
   }
 }
 </script>
@@ -57,10 +149,23 @@ export default class LayoutColumn extends Vue {
 <style lang="scss" scoped>
 .row {
   height: 100%;
+  position: relative;
 
   .col {
     border: solid 1px salmon;
     transition: background 0.3s;
+    text-align: center;
+  }
+
+  .splitter {
+    position: absolute;
+    top: 0;
+    width: 16px;
+    height: 100%;
+
+    //background: rgba(salmon, 0.4);
+    cursor: ew-resize;
+    transform: translateX(-50%);
   }
 
   .col:hover {
--- src/layouts/UiBuilderLayout.vue
+++ src/layouts/UiBuilderLayout.vue
@@ -1,7 +1,34 @@
 <template>
-  <div style="padding: 5em; height: 300px">
-    <LayoutColumn :editable="true" />
-  </div>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <q-toolbar class="bg-black text-white vue-draggable-handle">
+          <q-toolbar-title>Page Builder</q-toolbar-title>
+        </q-toolbar>
+        <div class="row">
+          <div class="col-2">
+            <q-list dark bordered separator draggable>
+              <q-item
+                draggable
+                @dragstart="startDrag($event, element)"
+                :key="element.id"
+                v-for="element in pageElements"
+              >
+                <q-item-section avatar>
+                  <q-icon color="white" :name="element.icon" />
+                </q-item-section>
+
+                <q-item-section>{{element.label}}</q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+          <div class="col-10 drop-zone" @drop="onDrop($event)" @dragover.prevent @dragenter.prevent>
+              <LayoutColumn v-for="i in rows + 1" />
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script lang='ts'>
@@ -15,14 +42,45 @@ import LayoutColumn from "components/ui_builder/LayoutColumn.vue";
   components: {
     GridLayout,
     GridItem,
-    LayoutColumn
+    LayoutColumn,
   },
 })
 export default class UiBuilderLayout extends Vue {
+    rows: number = 0;
+  pageElements = [
+    {
+      id: "column",
+      label: "Column",
+      icon: "las la-table",
+      defaultData: {},
+    },
+  ];
 
+  startDrag(evt: DragEvent, item: MetaFlowComponent) {
+    console.log(evt);
+    if (evt.dataTransfer === null) {
+      return;
+    }
+
+    evt.dataTransfer.dropEffect = "move";
+    evt.dataTransfer.effectAllowed = "move";
+    evt.dataTransfer.setData("itemId", item.id);
+
+    //console.log(item);
+  }
+
+  onDrop(evt: DragEvent) {
+    if (evt.dataTransfer === null) {
+      return;
+    }
+    const itemId = evt.dataTransfer.getData("itemId");
+
+    console.log(itemId);
+
+    this.rows += 1;
+  }
 }
 </script>
 
 <style lang='scss' scoped>
-
 </style>
